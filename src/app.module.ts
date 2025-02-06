c96ddab0765f5d83af074703c6ef3e574656ab73
Jimmy Andrade <contato@jimmyandrade.com>
2025-02-06 15:38:15 -0300

    feat: add `PrismaService` to app module providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { SMTPMailService } from './mail/smtp-mail.service';
 import { MailService } from './mail/mail.service';
 import { HttpModule } from './http.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   controllers: [AppController],
   imports: [HttpModule],
   providers: [
+    PrismaService,
     {
       provide: MailService,
       useClass: SMTPMailService,
